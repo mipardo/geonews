e5694e30ed42844a8c93ece6d69907de25b51146
al386105 <al386105@uji.es>
2021-11-03 23:01:37 +0100

    miguel integration tests done

--- app/src/test/java/es/uji/geonews/integration/R1_HU08.java
+++ app/src/test/java/es/uji/geonews/integration/R1_HU08.java
@@ -3,7 +3,6 @@ package es.uji.geonews.integration;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -11,8 +10,7 @@ import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
-import es.uji.geonews.model.GeographCoords;
-import es.uji.geonews.model.Location;
+import es.uji.geonews.model.LocationFactory;
 import es.uji.geonews.model.LocationManager;
 import es.uji.geonews.model.exceptions.GPSNotAvailableException;
 import es.uji.geonews.model.exceptions.NotValidCoordinatesException;
@@ -21,68 +19,58 @@ import es.uji.geonews.model.exceptions.UnrecognizedPlaceNameException;
 import es.uji.geonews.model.services.CoordsSearchService;
 import es.uji.geonews.model.services.ServiceManager;
 
-public class R1_HU02 {
+public class R1_HU08 {
 
     @Test
-    public void registerLocationByCoords_E1KnownPlaceName_Location()
+    public void getPlaceName_E1KnownCoords_nearestPlaceName()
             throws UnrecognizedPlaceNameException, ServiceNotAvailableException,
-            NotValidCoordinatesException, GPSNotAvailableException {
+            NotValidCoordinatesException {
         // Arrange
         CoordsSearchService mockCoordsSearchService = mock(CoordsSearchService.class);
         when(mockCoordsSearchService.isAvailable()).thenReturn(true);
         when(mockCoordsSearchService.getPlaceNameFromCoords(any())).thenReturn("Castellon de la Plana");
-        ServiceManager mockServiceManager = mock(ServiceManager.class);
-        when(mockServiceManager.getService("Geocode")).thenReturn(mockCoordsSearchService);
-        LocationManager locationManager = new LocationManager(mockServiceManager);
+        LocationFactory locationFactory = new LocationFactory(mockCoordsSearchService);
         // Act
-        locationManager.addLocation("39.98920, -0.03621");
+        //TODO: el metodo getPlaceFromCoords es privado y se usa al hacer el createLocation, ¿es correcto?
+        locationFactory.createLocation("39.98920, -0.03621");
         // Assert
         verify(mockCoordsSearchService, times(1)).isAvailable();
         verify(mockCoordsSearchService, times(1)).getPlaceNameFromCoords(any());
-        assertEquals(1, locationManager.getNonActiveLocations().size());
-        assertEquals(0, locationManager.getActiveLocations().size());
     }
 
 
     @Test
-    public void registerLocationByCoords_E2UnknownPlaceName_Location()
+    public void getPlaceName_E2UnknownCoords_nearestPlaceName()
             throws UnrecognizedPlaceNameException, ServiceNotAvailableException,
             NotValidCoordinatesException {
         // Arrange
         CoordsSearchService mockCoordsSearchService = mock(CoordsSearchService.class);
         when(mockCoordsSearchService.isAvailable()).thenReturn(true);
         when(mockCoordsSearchService.getPlaceNameFromCoords(any())).thenReturn(null);
-        ServiceManager mockServiceManager = mock(ServiceManager.class);
-        when(mockServiceManager.getService("Geocode")).thenReturn(mockCoordsSearchService);
-        LocationManager locationManager = new LocationManager(mockServiceManager);
+        LocationFactory locationFactory = new LocationFactory(mockCoordsSearchService);
         // Act
-        locationManager.addLocation("33.65001, -41.19001"); //TODO: Si lat o long acaban en 0 se trunca y da problemas
+        //TODO: el metodo getPlaceFromCoords es privado y se usa al hacer el createLocation, ¿es correcto?
+        locationFactory.createLocation("33.65001, -41.19001");
         // Assert
-        verify(mockCoordsSearchService, times(1)).getPlaceNameFromCoords(any());
         verify(mockCoordsSearchService, times(1)).isAvailable();
-        assertEquals(1, locationManager.getNonActiveLocations().size());
-        assertNull(locationManager.getNonActiveLocations().get(0).getPlaceName());
-        assertEquals(0, locationManager.getActiveLocations().size());
+        verify(mockCoordsSearchService, times(1)).getPlaceNameFromCoords(any());
     }
 
     @Test(expected= ServiceNotAvailableException.class)
-    public void registerLocationByCoords_E5WithoutConnection_ServiceNotAvailableException()
+    public void getPlaceName_E4GeocodeNotAvailable_ServiceNotAvailableException()
             throws UnrecognizedPlaceNameException, ServiceNotAvailableException,
             NotValidCoordinatesException {
         // Arrange
         CoordsSearchService mockCoordsSearchService = mock(CoordsSearchService.class);
         when(mockCoordsSearchService.isAvailable()).thenReturn(true);
         when(mockCoordsSearchService.getPlaceNameFromCoords(any())).thenThrow(new ServiceNotAvailableException());
-        ServiceManager mockServiceManager = mock(ServiceManager.class);
-        when(mockServiceManager.getService("Geocode")).thenReturn(mockCoordsSearchService);
-        LocationManager locationManager = new LocationManager(mockServiceManager);
+        LocationFactory locationFactory = new LocationFactory(mockCoordsSearchService);
         // Act
-        locationManager.addLocation("39.98920, -0.03621");
+        //TODO: el metodo getPlaceFromCoords es privado y se usa al hacer el createLocation, ¿es correcto?
+        locationFactory.createLocation("33.65001, -41.19001");
         // Assert
-        verify(mockCoordsSearchService, times(1)).getPlaceNameFromCoords(any());
         verify(mockCoordsSearchService, times(1)).isAvailable();
-        assertEquals(0, locationManager.getNonActiveLocations().size());
-        assertEquals(0, locationManager.getActiveLocations().size());
+        verify(mockCoordsSearchService, times(1)).getPlaceNameFromCoords(any());
     }
 
 }
