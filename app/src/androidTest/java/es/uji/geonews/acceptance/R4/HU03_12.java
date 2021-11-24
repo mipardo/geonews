44b9ec2a40ca918a5efa8c9aab2a6554dfef1b39
arturo <Calig2007>
2021-11-24 11:56:55 +0100

    added R4_HU12 E1

--- app/src/androidTest/java/es/uji/geonews/acceptance/R4/HU03_12.java
+++ app/src/androidTest/java/es/uji/geonews/acceptance/R4/HU03_12.java
@@ -1,6 +1,6 @@
 package es.uji.geonews.acceptance.R4;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import android.content.Context;
@@ -14,7 +14,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import es.uji.geonews.acceptance.AuxiliaryTestClass;
-import es.uji.geonews.model.Location;
 import es.uji.geonews.model.exceptions.NoLocationRegisteredException;
 import es.uji.geonews.model.exceptions.NotValidCoordinatesException;
 import es.uji.geonews.model.exceptions.ServiceNotAvailableException;
@@ -22,7 +21,7 @@ import es.uji.geonews.model.exceptions.UnrecognizedPlaceNameException;
 import es.uji.geonews.model.managers.GeoNewsManager;
 import es.uji.geonews.model.services.ServiceName;
 
-public class HU03_11 {
+public class HU03_12 {
     private GeoNewsManager geoNewsManager;
     private Context appContext;
 
@@ -34,7 +33,7 @@ public class HU03_11 {
     }
 
     @Test
-    public void activateService_localAndRemoteDatabasesAvailable_true()
+    public void deactivateService_localAndRemoteDatabasesAvailable_true()
             throws UnrecognizedPlaceNameException, ServiceNotAvailableException,
             NotValidCoordinatesException, InterruptedException, NoLocationRegisteredException {
         // Given
@@ -43,7 +42,7 @@ public class HU03_11 {
 
         // When
         CountDownLatch lock = new CountDownLatch(1);
-        boolean result = geoNewsManager.activateService(ServiceName.OPEN_WEATHER);
+        boolean result = geoNewsManager.deactivateService(ServiceName.AIR_VISUAL);
         lock.await(5000, TimeUnit.MILLISECONDS);
 
         // Then
@@ -51,7 +50,7 @@ public class HU03_11 {
         AuxiliaryTestClass.loadAll(loadedGeoNewsManager);
 
         assertTrue(result);
-        assertTrue(loadedGeoNewsManager.getService(ServiceName.OPEN_WEATHER).isActive());
-        assertTrue(loadedGeoNewsManager.getService(ServiceName.AIR_VISUAL).isActive());
+        assertFalse(loadedGeoNewsManager.getService(ServiceName.OPEN_WEATHER).isActive());
+        assertFalse(loadedGeoNewsManager.getService(ServiceName.AIR_VISUAL).isActive());
     }
 }
