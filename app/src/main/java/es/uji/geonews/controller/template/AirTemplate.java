3679ff3fadee263ea15c1b9ae34c7e3a81e1ae6b
al386105 <al386105@uji.es>
2021-12-05 23:24:06 +0100

    Basic Air Visual view done

---
+++ app/src/main/java/es/uji/geonews/controller/template/AirTemplate.java
@@ -0,0 +1,92 @@
+package es.uji.geonews.controller.template;
+
+import android.widget.TextView;
+
+public class AirTemplate {
+    private TextView tempertaureOutput;
+    private TextView preassureOutput;
+    private TextView humidityOutput;
+    private TextView windSpeedOutput;
+    private TextView windDirectionOuptut;
+    private TextView aqiUsOutput;
+    private TextView aqiCnOutput;
+    private TextView mainPollutantUsOutput;
+    private TextView getMainPollutantCnOutput;
+
+    public AirTemplate() {}
+
+    public TextView getTempertaureOutput() {
+        return tempertaureOutput;
+    }
+
+    public void setTempertaureOutput(TextView tempertaureOutput) {
+        this.tempertaureOutput = tempertaureOutput;
+    }
+
+    public TextView getPreassureOutput() {
+        return preassureOutput;
+    }
+
+    public void setPreassureOutput(TextView preassureOutput) {
+        this.preassureOutput = preassureOutput;
+    }
+
+    public TextView getHumidityOutput() {
+        return humidityOutput;
+    }
+
+    public void setHumidityOutput(TextView humidityOutput) {
+        this.humidityOutput = humidityOutput;
+    }
+
+    public TextView getWindSpeedOutput() {
+        return windSpeedOutput;
+    }
+
+    public void setWindSpeedOutput(TextView windSpeedOutput) {
+        this.windSpeedOutput = windSpeedOutput;
+    }
+
+    public TextView getWindDirectionOuptut() {
+        return windDirectionOuptut;
+    }
+
+    public void setWindDirectionOuptut(TextView windDirectionOuptut) {
+        this.windDirectionOuptut = windDirectionOuptut;
+    }
+
+    public TextView getAqiUsOutput() {
+        return aqiUsOutput;
+    }
+
+    public void setAqiUsOutput(TextView aqiUsOutput) {
+        this.aqiUsOutput = aqiUsOutput;
+    }
+
+    public TextView getAqiCnOutput() {
+        return aqiCnOutput;
+    }
+
+    public void setAqiCnOutput(TextView aqiCnOutput) {
+        this.aqiCnOutput = aqiCnOutput;
+    }
+
+    public TextView getMainPollutantUsOutput() {
+        return mainPollutantUsOutput;
+    }
+
+    public void setMainPollutantUsOutput(TextView mainPollutantUsOutput) {
+        this.mainPollutantUsOutput = mainPollutantUsOutput;
+    }
+
+    public TextView getGetMainPollutantCnOutput() {
+        return getMainPollutantCnOutput;
+    }
+
+    public void setGetMainPollutantCnOutput(TextView getMainPollutantCnOutput) {
+        this.getMainPollutantCnOutput = getMainPollutantCnOutput;
+    }
+
+
+
+}
